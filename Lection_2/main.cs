ba454124d2f8e9902c1f6416a21beb0953637564
Serejiy <nomi45148@gmail.com>
2024-06-18 18:16:12 +0200

    sha 256 algorithm on C#

---
+++ Lection_2/main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Your input = ");
+        string input = Console.ReadLine();
+        string hash = ComputeSha256Hash(input);
+        Console.WriteLine($"SHA-256 hash of '{input}' is: {hash}");
+    }
+
+    static string ComputeSha256Hash(string rawData)
+    {
+        // Создаем SHA256 объект
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            // Вычисляем хэш - возвращает байтовый массив
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            // Конвертируем байтовый массив в строку
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
