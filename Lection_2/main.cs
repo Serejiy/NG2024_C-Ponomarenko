1deefaf2b1dda60b1daf0ba7c0eec8024a05fd79
Serejiy <nomi45148@gmail.com>
2024-06-25 21:07:30 +0200

    russian comments deleted (sorry sir)

--- Lection_2/main.cs
+++ Lection_2/main.cs
@@ -14,13 +14,10 @@ class Program
 
     static string ComputeSha256Hash(string rawData)
     {
-        // Создаем SHA256 объект
         using (SHA256 sha256Hash = SHA256.Create())
         {
-            // Вычисляем хэш - возвращает байтовый массив
             byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
-            // Конвертируем байтовый массив в строку
             StringBuilder builder = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
             {
@@ -30,3 +27,4 @@ class Program
         }
     }
 }
+
